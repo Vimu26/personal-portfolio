a80ad290f8092db6fb35fa18d4bf960b89f2ce06
vimu26 <akalankavimukthi2@gmail.com>
2024-04-14 11:34:06 +0530

    icons adding completed

--- src/Pages/Skills/Skills.jsx
+++ src/Pages/Skills/Skills.jsx
@@ -1,44 +1,178 @@
 import * as React from "react";
 import Container from "@mui/material/Container";
 import { Grid, Button } from "@mui/material";
-import Typography from '@mui/material/Typography';
+import Typography from "@mui/material/Typography";
 
 const Skills = () => {
   return (
     <Container>
-           <Typography variant="h4" gutterBottom>
-       <b>Skills & Experience</b>
+      <Typography variant="h4" gutterBottom>
+        <b>Skills & Experience</b>
       </Typography>
       <Grid container spacing={2}>
         <Grid item xs={12} sm={6}>
-        <div>
+          <div>
             <h3>Languages and Tools</h3>
-            
           </div>
 
-          <div style={{display:"flex" , justifyContent : "start", alignItems:"center" , flexWrap: "wrap", }}>
-            <div style={{margin: '1rem '}}><img
-                  src="../../assets/images/skill-icons/icons8-typescript-96.png"
-                  alt=""
-                  style={{width: '70px' , height: '70px'}}
-              /></div>
-            <div style={{margin: '1rem '}}><img
-                  src="../../assets/images/skills/icons8-react-80.png"
-                  alt=""
-                  style={{width: '70px' , height: '70px'}}
-              /></div>
-            <div style={{margin: '1rem '}}>html</div>
-            <div style={{margin: '1rem '}}>html</div>
-            <div style={{margin: '1rem '}}>html</div>
-            <div style={{margin: '1rem '}}>html</div>
-            <div style={{margin: '1rem '}}>html</div>
-            <div style={{margin: '1rem '}}>html</div>
-            <div style={{margin: '1rem '}}>html</div>
-            <div style={{margin: '1rem '}}>html</div>
-
-
-
-
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "start",
+              alignItems: "center",
+              flexWrap: "wrap"
+            }}
+          >
+            <div style={{ margin: "1rem " }}>
+              <img
+                src={require("../../../src/assets/images/skill-icons/icons8-typescript-96.png")}
+                alt=""
+                style={{ width: "70px", height: "70px" }}
+              />
+            </div>
+            <div style={{ margin: "1rem " }}>
+              <img
+                src={require("../../../src/assets/images/skill-icons/icons8-javascript-96.png")}
+                alt=""
+                style={{ width: "70px", height: "70px" }}
+              />
+            </div>
+            <div style={{ margin: "1rem " }}>
+              {" "}
+              <img
+                src={require("../../../src/assets/images/skill-icons/icons8-java-96.png")}
+                alt=""
+                style={{ width: "70px", height: "70px" }}
+              />
+            </div>
+            <div style={{ margin: "1rem " }}>
+              {" "}
+              <img
+                src={require("../../../src/assets/images/skill-icons/html.png")}
+                alt=""
+                style={{ width: "70px", height: "70px" }}
+              />
+            </div>
+            <div style={{ margin: "1rem " }}>
+              {" "}
+              <img
+                src={require("../../../src/assets/images/skill-icons/icons8-css-96.png")}
+                alt=""
+                style={{ width: "70px", height: "70px" }}
+              />
+            </div>
+            <div style={{ margin: "1rem " }}>
+              {" "}
+              <img
+                src={require("../../../src/assets/images/skill-icons/icons8-c-96.png")}
+                alt=""
+                style={{ width: "70px", height: "70px" }}
+              />
+            </div>
+            <div style={{ margin: "1rem " }}>
+              {" "}
+              <img
+                src={require("../../../src/assets/images/skill-icons/icons8-angular-96.png")}
+                alt=""
+                style={{ width: "70px", height: "70px" }}
+              />
+            </div>
+            <div style={{ margin: "1rem " }}>
+              {" "}
+              <img
+                src={require("../../../src/assets/images/skills/icons8-react-80.png")}
+                alt=""
+                style={{ width: "70px", height: "70px" }}
+              />
+            </div>
+            <div style={{ margin: "1rem " }}>
+              {" "}
+              <img
+                src={require("../../../src/assets/images/skills/icons8-node-js-96.png")}
+                alt=""
+                style={{ width: "70px", height: "70px" }}
+              />
+            </div>
+            <div style={{ margin: "1rem " }}>
+              {" "}
+              <img
+                src={require("../../../src/assets/images/skill-icons/icons8-express-js-96.png")}
+                alt=""
+                style={{ width: "70px", height: "70px" }}
+              />
+            </div>
+            <div style={{ margin: "1rem " }}>
+              {" "}
+              <img
+                src={require("../../../src/assets/images/skill-icons/icons8-mongodb-96.png")}
+                alt=""
+                style={{ width: "70px", height: "70px" }}
+              />
+            </div>
+            <div style={{ margin: "1rem " }}>
+              {" "}
+              <img
+                src={require("../../../src/assets/images/skill-icons/icons8-mysql-96.png")}
+                alt=""
+                style={{ width: "70px", height: "70px" }}
+              />
+            </div>
+            <div style={{ margin: "1rem " }}>
+              {" "}
+              <img
+                src={require("../../../src/assets/images/skill-icons/icons8-express-js-96.png")}
+                alt=""
+                style={{ width: "70px", height: "70px" }}
+              />
+            </div>
+            <div style={{ margin: "1rem " }}>
+              {" "}
+              <img
+                src={require("../../../src/assets/images/skill-icons/icons8-nestjs-96.png")}
+                alt=""
+                style={{ width: "70px", height: "70px" }}
+              />
+            </div>
+            <div style={{ margin: "1rem " }}>
+              {" "}
+              <img
+                src={require("../../../src/assets/images/skills/icons8-redux-96.png")}
+                alt=""
+                style={{ width: "70px", height: "70px" }}
+              />
+            </div>
+            <div style={{ margin: "1rem " }}>
+              {" "}
+              <img
+                src={require("../../../src/assets/images/skill-icons/icons8-github-90.png")}
+                alt=""
+                style={{ width: "70px", height: "70px" }}
+              />
+            </div>
+            <div style={{ margin: "1rem " }}>
+              {" "}
+              <img
+                src={require("../../../src/assets/images/skill-icons/icons8-gitlab-96.png")}
+                alt=""
+                style={{ width: "70px", height: "70px" }}
+              />
+            </div>
+            <div style={{ margin: "1rem " }}>
+              {" "}
+              <img
+                src={require("../../../src/assets/images/skills/icons8-bootstrap-96.png")}
+                alt=""
+                style={{ width: "70px", height: "70px" }}
+              />
+            </div>
+            <div style={{ margin: "1rem " }}>
+              {" "}
+              <img
+                src={require("../../../src/assets/images/skills/mui.png")}
+                alt=""
+                style={{ width: "70px", height: "70px" }}
+              />
+            </div>
           </div>
         </Grid>
         <Grid item xs={12} sm={6}>
