d594a3677cbf7d58df2280a320cfcbc0cb7ee0bc
vimu26 <akalankavimukthi2@gmail.com>
2024-04-28 06:47:57 +0530

    contact component done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import About from "./Pages/About/About";
+import Contact from "./Pages/Contact/Contact";
 import Header from "./Pages/Header/Header";
 import Home from "./Pages/Home/Home";
 import Projects from "./Pages/Projects/Projects";
@@ -16,8 +17,11 @@ function App() {
         <div style={{ marginTop: "6rem", marginBottom: "3rem" }}>
           <Skills />
         </div>
-        <div style={{ marginTop: "6rem", marginBottom: "3rem" }}>
+        <div style={{ marginTop: "6rem", marginBottom: "5rem" }}>
           <Projects />
+        </div>
+        <div style={{ marginTop: "10rem", marginBottom: "10rem" }}>
+          <Contact />
         </div>
       </div>
     </div>
---
+++ src/Pages/Contact/Contact.jsx
@@ -0,0 +1,140 @@
+import React, { useState } from "react";
+import {
+  TextField,
+  Button,
+  Typography,
+  Snackbar,
+  Container,
+} from "@mui/material";
+import Grid from "@mui/material/Grid";
+import SendIcon from "@mui/icons-material/Send";
+// import axios from 'axios';
+
+const Contact = () => {
+  //   const theme = useTheme();
+  //   const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  const [formData, setFormData] = useState({
+    Fname: "",
+    Lname: "",
+    email: "",
+    message: "",
+  });
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+
+  const handleChange = (e) => {
+    console.log(e);
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async () => {
+    const { Fname, Lname, email, message } = formData;
+    const subject = "Mail from Portfolio website";
+    const to = "akalankavimukthi2@gmail.com";
+    const mailtoLink = `mailto:${to}?subject=${encodeURIComponent(
+      subject
+    )}&body=Name: ${Fname} ${Lname}%0D%0AEmail: ${email}%0D%0AMessage: ${message}`;
+
+    window.location.href = mailtoLink;
+
+    setFormData({ Fname: "", Lname: "", email: "", message: "" });
+    setSnackbarOpen(true);
+  };
+
+  const handleCloseSnackbar = () => {
+    setSnackbarOpen(false);
+  };
+  return (
+    <Container maxWidth="xl">
+      <Typography variant="h4" align={"center"}>
+        <b>Contact Me</b>
+      </Typography>
+      <Typography
+        variant="subtitle1"
+        align={"center"}
+        sx={{
+          color: "grey",
+        }}
+      >
+        Need to Hire? Please Feel Free to send a Message <br></br>
+        I'll get Touch with you as Soon as Possible.
+      </Typography>
+
+      <div style={{ padding: "20px" }}>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <TextField
+              name="Fname"
+              label="First Name"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+              value={formData.Fname}
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              name="Lname"
+              label="Last Name"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+              value={formData.Lname}
+              onChange={handleChange}
+            />
+          </Grid>
+        </Grid>
+
+        <TextField
+          name="email"
+          label="Email"
+          variant="outlined"
+          fullWidth
+          margin="normal"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <TextField
+          name="message"
+          label="Message"
+          variant="outlined"
+          fullWidth
+          multiline
+          rows={4}
+          margin="normal"
+          value={formData.message}
+          onChange={handleChange}
+        />
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <Button
+            style={{ width: "100%", height: "4rem" }}
+            variant="contained"
+            color="primary"
+            onClick={handleSubmit}
+            endIcon={<SendIcon />}
+          >
+            <b>Send</b>
+          </Button>
+        </div>
+
+        <Snackbar
+          open={snackbarOpen}
+          autoHideDuration={6000}
+          onClose={handleCloseSnackbar}
+          message="Message sent successfully!"
+          action={
+            <Button
+              color="inherit"
+              size="small"
+              onClick={() => handleCloseSnackbar}
+            >
+              Close
+            </Button>
+          }
+        />
+      </div>
+    </Container>
+  );
+};
+
+export default Contact;
